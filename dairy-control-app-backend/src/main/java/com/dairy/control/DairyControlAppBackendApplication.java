aeca65c321c325f2c9f3ffa0831ae2467b637048
Martin Beltramino <martin1484@gmail.com>
2017-11-14 21:23:46 -0300

    First Commit

---
+++ dairy-control-app-backend/src/main/java/com/dairy/control/DairyControlAppBackendApplication.java
@@ -0,0 +1,12 @@
+package com.dairy.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyControlAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyControlAppBackendApplication.class, args);
+	}
+}
---
+++ dairy-control-app-backend/src/test/java/com/dairy/control/DairyControlAppBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dairy.control;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DairyControlAppBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
